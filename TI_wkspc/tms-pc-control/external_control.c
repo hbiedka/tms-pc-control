312b794ded616ad27e0f94542dc3edc74ebe2a0a
Bogdan Bednarski <b.bednarski@nevomo.tech>
2021-01-20 15:34:53 +0100

    Strukturka do nadzoru TI

---
+++ TI_wkspc/tms-pc-control/external_control.c
@@ -0,0 +1,41 @@
+/*
+ * external_control.c
+ *
+ *  Created on: 20 sty 2021
+ *      Author: Bogdan
+ */
+
+struct {
+    short pb_gpio[2];
+    short enc_gpio;
+
+    short led_gpio[2];
+
+    float vr_adc[2];
+
+    float pwm_freq[6];
+    float pwm_duty[6];
+
+    float tim_freq[2];
+}TMS_state;
+
+void updateState(TMS_state state){
+
+}
+
+void setLED(short index,short state){
+    //TODO
+}
+
+void setPWMduty(short index, float dc){
+
+}
+
+void setPWMduty(short index, float freq){
+
+}
+
+void setTimerFreq(short index, float freq){
+
+}
+
---
+++ TI_wkspc/tms-pc-control/master_header.h
@@ -0,0 +1,13 @@
+/*
+ * master_header.h
+ *
+ *  Created on: 20 sty 2021
+ *      Author: Bogdan
+ */
+
+#ifndef MASTER_HEADER_H_
+#define MASTER_HEADER_H_
+
+
+
+#endif /* MASTER_HEADER_H_ */
